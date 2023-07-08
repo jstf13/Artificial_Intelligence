13fd2f3a81bf9b37956a20c9c9faf56a0a421b1e
Daniel Paggiola <dpaggiola@gmail.com>
2023-07-07 21:21:59 -0300

    Se limpia y corrige el Connect Four

--- ConnectFour/Agent_Minimax.py
+++ ConnectFour/Agent_Minimax.py
@@ -1,12 +1,11 @@
 from board import Board
 from agent import Agent
 
-PLAYER_X = "X"
-PLAYER_O = "O"
+player_one = 1
+player_two = 2
+x = "x"
+y = "y"
 
-'''     def __init__(self, player=1):
-        super().__init__(player)
- '''
 class AgentMinimax(Agent):
     def next_action(self, obs):
         while True:
@@ -16,101 +15,23 @@ class AgentMinimax(Agent):
             except ValueError:
                 print("Please insert a number.")
 
-    '''
-    Debemos definir la función de evaluación (heuristic_utility) que sea buena
-    y que cumpla con los criterios que vimos en el teórico.
-
-    El heuristic_utility ya recibe un tablero (board) actual, donde debemos clonar
-    y llamar recurrentemente para poder obtener el próximo estado que nos maximice
-    ganar.
-    '''
     def heuristic_utility(self, board: Board):
-        if board.is_winning_cell((board._last_modified_cell)):
-            return float('inf') if board.winner == PLAYER_X else float('-inf')
+        if board.is_winning_cell((board._last_modified_cell[x], board._last_modified_cell[y])):
+            return float('inf') if board.winner == player_one else float('-inf')
 
         if board.is_full():
             return 0
 
         valid_moves = board.get_posible_actions()
-        best_score = float('-inf') if board._get_player_str == PLAYER_X else float('inf')
+        best_score = float('-inf') if board._get_player_str == player_one else float('inf')
 
         for col in valid_moves:
             temp_board = board.clone()
-            temp_board.add_tile(col, board.current_player)
+            temp_board.add_tile(col, board._get_player_str)
             score = self.heuristic_utility(temp_board)
-            if board._get_player_str == PLAYER_X:
+            if board._get_player_str == player_one:
                 best_score = max(score, best_score)
             else:
                 best_score = min(score, best_score)
 
         return best_score
-
-
-    
-'''         ended = board.is_final()
-        if ended:
-            winner = board.winner()
-            if winner == self.player:
-                return 1
-            if winner == self.other_player:
-                return -1
-            else:
-                return 0
-        #Casos no base
-        actions = board.get_posible_actions()
-        random.shuffle(actions)
-        action_nodes = []
-        for action in actions:
-            child_node = board.clone()
-            action_nodes.append((action, child_node))
-            value = min(value, heuristic_utility(child_node)) '''
-
-'''     def policy(self, board: Board) -> tuple[int, int]:
-        self.idx = 0
-        pos, _ = self.minimax(board, self.player)
-        return pos '''
-
-'''     def minimax(self, board: Board, player: int)-> tuple[tuple[int, int], int]:
-        #Caso base
-        ended, winner = board.is_end()
-        if ended:
-            if winner == self.player:
-                return None, 1
-            if winner == self.other_player:
-                return None, -1
-            else:
-                return None, 0
-
-        #Casos no base
-        actions = board.get_available_cells()
-        random.shuffle(actions)
-        action_nodes = []
-        for action in actions:
-            child_node = board.clone()
-            child_node.play(action, player)
-            action_nodes.append((action, child_node))
-
-        value = 0
-        chosen_action = None  
-        
-        if player != self.player: # mini
-            #Buscar acción que minimiza el valor
-            #Del chatGPT sale:
-            value = float('inf')
-            for col in valid_moves:
-                temp_board = board.copy()
-                row = get_next_open_row(temp_board, col)
-                drop_piece(temp_board, row, col, PLAYER_O)
-                value = min(value, minimax(temp_board, depth - 1, True))
-
-        else: #max (player == self.player)
-            #Buscar acción que maximiza el valor
-            #Del chatGPT sale:
-            value = float('-inf')
-            for col in valid_moves:
-                temp_board = board.copy()
-                row = get_next_open_row(temp_board, col)
-                drop_piece(temp_board, row, col, PLAYER_X)
-                value = max(value, minimax(temp_board, depth - 1, False))
-
-        return chosen_action, value '''
